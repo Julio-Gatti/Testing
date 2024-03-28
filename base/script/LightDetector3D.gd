1a52129e3373680c0b04cc6810f60d9297751cb5
Julius Sakki <j.sakki@icloud.com>
2024-03-28 19:55:29 +0200

    Add BlobShadow3D.gd and LightDetector3D.gd

---
+++ base/script/LightDetector3D.gd
@@ -0,0 +1,15 @@
+# Detects how lit some point is.
+# TODO determine collision layer for "light", so it can pass through
+# translucent stuff, etc.
+class_name LightDetector3D
+extends VisualInstance3D
+
+# TODO cache light nodes somewhere...
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
